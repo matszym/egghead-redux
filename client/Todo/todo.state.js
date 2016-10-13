33dd248630655c5ae12c7812b45f6008f6b3c175
Mateusz Szymański <matszym@users.noreply.github.com>
2016-10-13 14:38:28 +0200

    Refactor reducer, separate concerns

--- client/Todo/todo.state.js
+++ client/Todo/todo.state.js
@@ -1,31 +1,31 @@
-const todo = (state = [], action) => {
+const todo = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return Object.assign({}, action.payload, {completed: false})
+    case 'TOGGLE_TODO':
+      if (state.id !== action.payload.id) {
+        return Object.assign({}, state);
+      }
+
+      return Object.assign({}, state, {completed: !state.completed})
+    default:
+      return Object.assign({}, state);
+  }
+}
+
+const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        Object.assign(
-          {}, 
-          action.payload,
-          {
-            completed: false
-          })
+        todo(state, action)
       ];
     case 'TOGGLE_TODO': 
-      return [
-        ...state
-          .map(task => {
-            let toggleCompleted = {};
-
-            if (task.id === action.payload.id) {
-              toggleCompleted.completed = !task.completed;
-            }
-
-            return Object.assign({}, task, toggleCompleted);
-          })
-      ];
+      return state
+          .map(task => todo(task, action));
     default:
       return [...state];
   }
 }
 
-export default todo;
+export default todos;
