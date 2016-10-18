08de96c39fe273b806e0270c05fe7b535b9bc274
Mateusz Szymański <matszym@users.noreply.github.com>
2016-10-18 20:39:00 +0200

    Use react-redux bindings instead of custom Provider component

--- client/RenderWebsite.js
+++ client/RenderWebsite.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Provider from './Todo/Provider';
+import {Provider} from 'react-redux';
 import TodoComponent from './Todo/todo';
 import {todoApp, store} from './Todo/todo.state';
 import './Counter/counter.css';
